120e16d82ecf05d1c15ad52755b43a4dff31f7f2
Piercarlo Serena <piercarlo.serena@gmail.com>
2026-03-26 21:41:28 +0100

    feat: enhance app configuration with HTTP client and interceptors

--- apps/auth/src/app/app.config.ts
+++ apps/auth/src/app/app.config.ts
@@ -6,13 +6,29 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptors,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { AuthHttpInterceptor } from '@auth0/auth0-angular';
 import { appRoutes } from './app.routes';
+import { API_BASE_URL } from '@saas-frontend/shared/util-types';
+import { tenantInterceptor } from '@saas-frontend/organizations/data-access';
+import { environment } from 'src/environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideAnimationsAsync(),
     provideRouter(appRoutes),
+    provideHttpClient(
+      withInterceptorsFromDi(),
+      withInterceptors([tenantInterceptor]),
+    ),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true },
+    { provide: API_BASE_URL, useValue: environment.apiUrl },
     providePrimeNG({
       theme: { preset: Aura, options: { darkModeSelector: 'false' } },
     }),
